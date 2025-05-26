b66bca02a2a17dbcff899486420570dce3ef6695
TurbulenceChaos <turbulencechaos@gmail.com>
2025-05-27 01:19:11 +0800

    Add `sci-wolfram-jupyter-eval-region-or-buffer` function. Add an option of copying symbols from Data-example folder

--- sci-wolfram-all-symbols.el
+++ sci-wolfram-all-symbols.el
@@ -10,16 +10,38 @@
 (let* ((dir (file-name-directory (or load-file-name buffer-file-name)))
        (data-dir (concat dir "Data")))
   (when (not (file-directory-p data-dir))
-    (message "Created wolfram symbols dir.")
+    (message "Creat wolfram symbols dir")
     (make-directory data-dir))
   (when (not (directory-files data-dir nil "\\.wl\\'"))
-    (message (format "Copy LSPServer wolfram symbols to %s." data-dir))
     (let ((lsp-server-path
 	   (string-trim-right
-	    (shell-command-to-string "wolframscript -code 'LSPServer = #[\"Location\"]& /@ PacletFind[\"LSPServer\"]; LSPServerBuiltIn = Select[LSPServer, StringContainsQ[#, $InstallationDirectory] &][[1]]; FileNameJoin[{LSPServerBuiltIn, \"Resources\",\"Data\"}]'"))))
-      (copy-directory lsp-server-path dir)))
-  (if (not (directory-files data-dir nil "\\.el\\'"))
-      (shell-command (format "wolframscript -script %s %s" sci-wolfram-all-symbols-script dir))))
+	    (shell-command-to-string
+	     (concat
+	      "wolframscript -code "
+	      "'"
+	      "LSPServer = #[\"Location\"]& /@ PacletFind[\"LSPServer\"]; "
+	      "LSPServerBuiltIn = Select[LSPServer, StringContainsQ[#, $InstallationDirectory] &][[1]]; "
+	      "FileNameJoin[{LSPServerBuiltIn, \"Resources\",\"Data\"}]"
+	      "'")))))
+      (if (yes-or-no-p
+	   (format
+	    "Wolfram LSPServer symbols not found in the Data folder!
+Do you want to copy wolfram symbols from built-in LSPServer package in %s?
+If not, copy them from the Data-example folder."
+	    lsp-server-path))
+	  (progn
+	    (message (format "Copy built-in wolfram LSPServer symbols to %s" data-dir))
+	    (copy-directory lsp-server-path dir nil t)
+	    (if (not (directory-files data-dir nil "\\.el\\'"))
+		(shell-command
+		 (format
+		  "wolframscript -script %s %s"
+		  sci-wolfram-all-symbols-script
+		  dir))))
+	(copy-directory
+	 (expand-file-name "Data-example" dir)
+	 (expand-file-name "Data" dir)
+	 nil t)))))
 
 (add-to-list 'load-path (expand-file-name "Data" (file-name-directory (or load-file-name buffer-file-name))))
 (require 'SystemLongNames)
